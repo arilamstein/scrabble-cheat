eab309ede76027e227444c964b64179fb56d3502
Ari Lamstein <arilamstein@gmail.com>
2020-04-19 10:33:41 -0700

    Remove "requirements" as a string, and replace it with a series of more explicit parameters.

--- scrabble-cheat.py
+++ scrabble-cheat.py
@@ -1,27 +1,15 @@
-def load_words():
+# the longest word in the dictionary is dichlorodiphenyltrichloroethane,
+# which has 31 letters. But the max length of the board in WWF is 15
+# also, WWF does not allow 1-letter words
+def load_words(max_length=15):
     with open('words_alpha.txt') as word_file:
-        valid_words = set(word_file.read().split())
+        return [one_word
+                for one_word in set(word_file.read().split())
+                if (len(one_word) > 1) and (len(one_word) <= max_length)]
 
-    return sorted(valid_words)
 
-
-def can_spell_word(word, tiles, requirements):
-    # 1 letter words are not valid
-    if len(word) == 1:
-        return False
-
-    if requirements is None:
-        requirements = '*' * len(word)
-    elif len(word) != len(requirements):
-        return False
-
-    for one_requirement, one_letter in zip(requirements, word):
-        # if we are required to play a certain letter here,
-        # abort if the word does not meet the requirement
-        if one_requirement != '*':
-            if one_requirement != one_letter:
-                return False
-
+def can_spell_word(word, tiles):
+    for one_letter in word:
         # if this letter is available in our tiles, remove it and continue
         if one_letter in tiles:
             tiles = tiles.replace(one_letter, '', 1)
@@ -44,15 +32,34 @@ def get_word_score(word):
                 for letter in word])
 
 
-# requirements is a string like '***s', which means
-# 'requires a "s" at the end of a 4 letter word',
-# ex 'cats' and 'dogs'
-def get_all_possible_words(tiles, requirements=None):
+def word_has_letters(word, required_letters):
+    if not required_letters:
+        return True
+    else:
+        return all(one_letter in word
+                   for one_letter in required_letters)
+
+
+# here we assume that "tiles" are the tiles that the user has
+# "starts_with" and "ends_with" are considered to be tiles that are already on the board
+def get_all_words(tiles, required_letters="", max_length=15, starts_with="", ends_with=""):
+    tiles += starts_with + ends_with
+    words = [(one_word, get_word_score(one_word))
+             for one_word in load_words(max_length=max_length)
+             if one_word.startswith(starts_with) and
+             one_word.endswith(ends_with) and
+             word_has_letters(one_word, required_letters) and
+             can_spell_word(one_word, tiles)]
+
+    return sorted(words, key=lambda x: x[1], reverse=True)
+
+
+def starts_with_ends_with(starts_with="", ends_with=""):
     words = [(one_word, get_word_score(one_word))
              for one_word in load_words()
-             if can_spell_word(one_word, tiles, requirements)]
+             if one_word.startswith(starts_with) and one_word.ends_with(ends_with)]
 
     return sorted(words, key=lambda x: x[1], reverse=True)
 
 # Print an example
-print(get_all_possible_words('crtswyo'))
+get_all_words('ilzsiwl', max_length=7, ends_with='i')
